e3c9c5eaa919e84653153c5a82fd07ef7fdc79c6
streamferow <ivanpanferov2016@mail.ru>
2025-09-20 01:16:44 +0300

    added load functions

--- data/raw.py
+++ data/raw.py
@@ -1,9 +1,16 @@
 import pandas as pd
 
-MEDIA_PATH = ""
-SERIES_PATH = ""
+PRICE_COLS = ["open_price", "close_price", "high_price", "low_price"]
+SENTIMENT_COLS = ["articles"]
 
 def load_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
 
+def get_price_df(df: pd.DataFrame) -> pd.DataFrame:
+    price_df = df[PRICE_COLS].copy()
+    return price_df
+
+def get_sentiment_df(df: pd.DataFrame) -> pd.DataFrame:
+    sentiment_df = df[SENTIMENT_COLS]
+    return sentiment_df
