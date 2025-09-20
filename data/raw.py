16a71208c574ca08a81193b645bf33ce7aa62d3e
streamferow <ivanpanferov2016@mail.ru>
2025-09-20 10:27:05 +0300

    finished loading file

--- data/raw.py
+++ data/raw.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-PRICE_COLS = ["open_price", "close_price", "high_price", "low_price"]
-SENTIMENT_COLS = ["articles"]
+DATE_COL = ["begins_at"]
+PRICE_COLS = ["open_price", "close_price", "high_price", "low_price"] + DATE_COL
+SENTIMENT_COLS = ["articles"] + DATE_COL
 
 def load_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
 
-def get_price_df(df: pd.DataFrame) -> pd.DataFrame:
+def get_price_df(path: str) -> pd.DataFrame:
+    df = load_data(path)
     price_df = df[PRICE_COLS].copy()
     return price_df
 
-def get_sentiment_df(df: pd.DataFrame) -> pd.DataFrame:
-    sentiment_df = df[SENTIMENT_COLS]
+def get_sentiment_df(path: str) -> pd.DataFrame:
+    df = load_data(path)
+    sentiment_df = df[SENTIMENT_COLS].copy()
     return sentiment_df
